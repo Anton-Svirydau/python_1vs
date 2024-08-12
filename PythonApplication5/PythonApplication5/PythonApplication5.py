3922740422c56b1cbcc75702ff617a25dc2240cb
Alexy Sviridov <unskillton@gmail.com>
2024-08-12 08:02:10 +0300

    5, 6

---
+++ PythonApplication5/PythonApplication5/PythonApplication5.py
@@ -0,0 +1,89 @@
+'''
+print(min(1, 2, 3))
+print(max(1, 2, 3))
+print(abs(5))
+print(abs(-5))
+print(len('Naruto'))
+'''
+
+'''
+a = int(input())
+
+print(min(3 * a, 2 * a + 100))
+'''
+
+'''
+a = input()
+b = input()
+
+print(max(len(a), len(b)))
+'''
+
+'''
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+print(max(abs(a - b), abs(b - c), abs(c - d)))
+'''
+
+import math
+
+''' 
+print(math.gcd(17, 51))
+print(math.factorial(5))
+
+n = int(input())
+
+print(math.factorial(n))
+'''
+
+'''
+s = 'I know Python'
+
+print(s.title())
+print(s.capitalize())
+print(s.lower())
+print(s.upper())
+print(s)
+'''
+
+'''
+print('Hello!'.upper())
+'''
+
+'''
+print('  aba '.strip())
+print(' a b '.strip())
+print('kek'.strip())
+'''
+
+'''
+print('aabcaaacba'.strip('a'))
+print('baacabcba'.strip('ab'))
+print('   bro   '.strip('r'))
+'''
+
+'''
+print('aba'.replace('a', 'c'))
+print('Hello, Alisa!'.replace('Alisa', 'Bob'))
+print('aaaa'.replace('a', 'b', 2))
+'''
+
+'''
+a = int(input())
+s = input()
+
+if a == 1:
+	print(s.lower())
+elif a == 2:
+	print(s.upper())
+'''
+
+'''
+a = input()
+
+print(a.strip().replace('z', 's').replace('Z','S'))
+'''
+
