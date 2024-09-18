ac5b2d0ecc8d66672a97cb161fdaabb1967e9ee7
Alexy Sviridov <unskillton@gmail.com>
2024-09-18 09:35:53 +0300

    Create Binary_Search.pyproj

--- PythonApplication18/PythonApplication18/PythonApplication18.py
+++ PythonApplication18/PythonApplication18/PythonApplication18.py
@@ -151,7 +151,7 @@ t2 = (1, 2)
 
 print(t1 < t2)  
 '''
-
+'''
 from collections import defaultdict
 
 pixels = defaultdict(list)
@@ -188,7 +188,53 @@ for _ in range(n):
         erase_pixel(x, y)
 
 print("\n".join(results))
+'''
+'''
+def binary_search(list, start_element, key):
+    end_element = len(list) - 1
+    while start_element <= end_element:
+        middle_element = start_element + (end_element - start_element) // 2
+        if list[middle_element] == key:
+            return middle_element
+        elif list[middle_element] < key:
+            start_element = middle_element + 1
+        else:
+            end_element = middle_element - 1
+    return -1
 
+
+sequence = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30]
+
+find_element = 24
+
+result = binary_search(list=sequence, start_element=0, key=find_element)
+print(result)
+'''
+
+def binary_search(list, item):
+    low = 0
+    high = len(list) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = list[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    
+    return "No item in list"
+
+
+my_list = [1, 3, 5, 7, 9, 11, 13, 14, 15, 17, 19, 21]
+
+result_1 = binary_search(my_list, 17)
+result_2 = binary_search(my_list, 12)
+print(result_1)
+print(result_2)
 
 
 
